d95417333e2782367925e6d119691679059999c3
José María Gutiérrez <josemariagutierrez@u2204wsl2jmgr.dir.svc.accenture.com>
2022-09-15 09:17:41 +0200

    Testing conflicts

--- vars/isDevelopBranch.groovy
+++ vars/isDevelopBranch.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String branch) {
-    return "develop".equals(branch)
+    // return "develop".equals(branch)
+    return "==>==>Invocación desde libreria maven1<==<=="
 }
